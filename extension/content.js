ac91cfbba3cdd4d47c9a888b61c3cf0abb35a29b
TrishnaT24 <tiwaritrishna07@gmail.com>
2025-02-23 14:48:51 +0530

    extract all

--- extension/content.js
+++ extension/content.js
@@ -1,23 +1,80 @@
 
-function findCodeforcesHandle() {
-    // Try multiple selectors to find the handle
-    const selectors = [
-        "a[href^='/profile/']",
-        '.lang-chooser span a',
-        '.rated-user',
-        '#header .username'
-    ];
+// function findCodeforcesHandle() {
+//     // Try multiple selectors to find the handle
+//     const selectors = [
+//         "a[href^='/profile/']",
+//         '.lang-chooser span a',
+//         '.rated-user',
+//         '#header .username'
+//     ];
 
-    for (let selector of selectors) {
-        const element = document.querySelector(selector);
-        if (element) {
-            return element.innerText.trim();
-        }
+//     for (let selector of selectors) {
+//         const element = document.querySelector(selector);
+//         if (element) {
+//             return element.innerText.trim();
+//         }
+//     }
+//     return null;
+// }
+
+// // Run when DOM is ready
+// function init() {
+//     console.log("Content script initializing...");
+    
+//     const handle = findCodeforcesHandle();
+//     if (handle) {
+//         console.log("Found Codeforces handle:", handle);
+        
+//         chrome.runtime.sendMessage(
+//             { action: "sendHandle", handle: handle },
+//             response => {
+//                 if (chrome.runtime.lastError) {
+//                     console.error("Message sending error:", chrome.runtime.lastError);
+//                     return;
+//                 }
+                
+//                 console.log("Background script response:", response);
+//                 if (response?.success) {
+//                     console.log("Recommendations received successfully");
+//                 } else {
+//                     console.error("Error getting recommendations:", response?.error);
+//                 }
+//             }
+//         );
+//     } else {
+//         console.error("Could not find Codeforces handle");
+//     }
+// }
+
+// // Try both DOMContentLoaded and load events
+// document.addEventListener("DOMContentLoaded", init);
+// if (document.readyState === "complete") {
+//     init();
+// }
+
+
+
+
+
+
+function findCodeforcesHandle() {
+    // First check URL for profile page
+    let url = window.location.pathname;
+    let match = url.match(/^\/profile\/(.+)$/);
+    
+    if (match) {
+        return match[1];
     }
+    
+    // Try to find handle from profile link
+    let profileLink = document.querySelector("a[href^='/profile/']");
+    if (profileLink) {
+        return profileLink.getAttribute("href").split("/").pop();
+    }
+    
     return null;
 }
 
-// Run when DOM is ready
 function init() {
     console.log("Content script initializing...");
     
--- extension/popup.js
+++ extension/popup.js
@@ -1,54 +1,104 @@
-document.getElementById("sendHandle").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (!tabs[0]?.id) {
-      console.error("No active tab found");
-      return;
-    }
 
-    chrome.scripting
-      .executeScript({
-        target: { tabId: tabs[0].id },
-        function: extractHandle,
-      })
-      .then((result) => {
-        console.log("Script execution result:", result);
-      })
-      .catch((err) => {
-        console.error("Script execution error:", err);
-      });
-  });
+
+// document.getElementById("sendHandle").addEventListener("click", () => {
+//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//         if (!tabs[0]?.id) {
+//             console.error("No active tab found");
+//             return;
+//         }
+
+//         chrome.scripting.executeScript({
+//             target: { tabId: tabs[0].id },
+//             function: extractHandle
+//         }).then(result => {
+//             console.log("Script execution result:", result);
+//         }).catch(err => {
+//             console.error("Script execution error:", err);
+//         });
+//     });
+// });
+
+// function extractHandle() {
+//     let url = window.location.pathname;
+//     let match = url.match(/^\/profile\/(.+)$/);
+    
+//     if (match) {
+//         let handle = match[1];
+//         console.log("Extracted handle:", handle);
+        
+//         // Send message and wait for response
+//         chrome.runtime.sendMessage(
+//             { action: "sendHandle", handle: handle },
+//             response => {
+//                 console.log("Background script response:", response);
+//                 if (response?.success) {
+//                     alert("Recommendations received successfully!");
+//                 } else {
+//                     alert("Error getting recommendations. Check console for details.");
+//                 }
+//             }
+//         );
+//     } else {
+//         alert("No handle found! Please go to a Codeforces profile page.");
+//     }
+// }
+
+
+
+
+
+document.getElementById("sendHandle").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (!tabs[0]?.id) {
+            console.error("No active tab found");
+            return;
+        }
+        
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: extractHandle
+        }).then(result => {
+            console.log("Script execution result:", result);
+        }).catch(err => {
+            console.error("Script execution error:", err);
+        });
+    });
 });
 
-function extractHandle() {
-  let url = window.location.pathname;
-  let match = url.match(/^\/profile\/(.+)$/);
-
-  if (match) {
-    let handle = match[1];
+function sendHandleToBackground(handle) {
     console.log("Extracted handle:", handle);
-  }
-  let profileLink = document.querySelector("a[href^='/profile/']");
-  if (profileLink) {
-    let handle = profileLink.getAttribute("href").split("/").pop();
-    console.log("Your handle:", handle);
-  } else {
-    console.log("Profile link not found!");
-  }
-
-  if (handle) {
-    // Send message and wait for response
+    
     chrome.runtime.sendMessage(
-      { action: "sendHandle", handle: handle },
-      (response) => {
-        console.log("Background script response:", response);
-        if (response?.success) {
-          alert("Recommendations received successfully!");
-        } else {
-          alert("Error getting recommendations. Check console for details.");
+        { action: "sendHandle", handle: handle },
+        response => {
+            console.log("Background script response:", response);
+            if (response?.success) {
+                alert("Recommendations received successfully!");
+            } else {
+                alert("Error getting recommendations. Check console for details.");
+            }
         }
-      }
     );
-  } else {
-    alert("No handle found! Please go to a Codeforces profile page.");
-  }
+    
+    return handle;
+}
+function extractHandle() {
+    // First try to get handle from URL if on profile page
+    let url = window.location.pathname;
+    let match = url.match(/^\/profile\/(.+)$/);
+    
+    if (match) {
+        let handle = match[1];
+        return sendHandleToBackground(handle);
+    }
+    
+    // Try to find handle from profile link
+    let profileLink = document.querySelector("a[href^='/profile/']");
+    if (profileLink) {
+        let handle = profileLink.getAttribute("href").split("/").pop();
+        return sendHandleToBackground(handle);
+    }
+    
+    alert("No handle found! Ensure you are logged in.");
+    return null;
 }
